f4f9cc0202e774888da4ebe2b1bbb282c824e57a
mahekshk <shaikhmahek427@gmail.com>
2026-05-14 15:03:56 +0530

    single link deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -249,7 +249,7 @@ const paymentRazorpay = async (req, res) => {
         // creating options for razorpay payment
         const options = {
             amount: appointmentData.amount * 100,
-            currency: process.env.CURRENCY,
+            currency: "INR",
             receipt: appointmentId,
         }
 
